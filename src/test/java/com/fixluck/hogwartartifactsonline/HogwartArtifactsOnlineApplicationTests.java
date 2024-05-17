1d1614a76143ad879ef68f4e72520ba3b86f5bb9
FixLuck <caube.vodoi5@gmail.com>
2024-05-17 20:06:25 +0700

    This is the first commit

---
+++ src/test/java/com/fixluck/hogwartartifactsonline/HogwartArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fixluck.hogwartartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartArtifactsOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
